20041f119cd07938133e958d30a5c076daef5a47
Ben Langfeld <ben@langfeld.me>
2013-12-10 22:38:10 -0500

    Install Asterisk 12 beta2

--- roles/asterisk.rb
+++ roles/asterisk.rb
@@ -4,6 +4,8 @@ run_list "recipe[asterisk]",
   "recipe[asterisk::unimrcp]"
 
 override_attributes asterisk: {
+  version: '12.0.0-beta2',
+  checksum: "dbc1c06ba96127473f96f3af808a60db1654bd06cff907bbb11a09e443b90f4b",
   sip: {
     context: 'adhearsion'
   },
