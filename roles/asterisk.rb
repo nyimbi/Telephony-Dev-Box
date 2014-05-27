8a4c934561b3a2a261c52ac713358cc96129a0a8
Ben Langfeld <ben@langfeld.me>
2014-05-27 20:22:15 -0300

    Correctly set Asterisk version

--- roles/asterisk.rb
+++ roles/asterisk.rb
@@ -6,8 +6,10 @@ run_list "recipe[asterisk]",
   "recipe[asterisk::unimrcp]"
 
 override_attributes asterisk: {
-  version: '12.0.0',
-  checksum: '0efc4507d5384447011fd1148eaaae2d06f70b04c736b4c73fbb9941a9440a2b',
+  source: {
+    version: '12.0.0',
+    checksum: '0efc4507d5384447011fd1148eaaae2d06f70b04c736b4c73fbb9941a9440a2b',
+  },
   sip: {
     context: 'adhearsion'
   },
